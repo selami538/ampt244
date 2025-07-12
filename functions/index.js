0e5aef4ce55a8e07049f70617e838fcfd67f8ff4
selami538 <erdaltayfun37@gmail.com>
2025-07-12 22:48:54 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,20 @@
-function translateResponseLocation(headers) {
-  const newHeaders = new Headers(headers);
- //Delete Cloudflare default access control allow origin hea12dersssa
-  if (headers.has("*")) {
-       newHeaders.delete("*")       
-  }
-  newHeaders.delete("*") 
-  newHeaders.delete("set-cookie")
- //Now add our domain access control allossw origin 234d
-  newHeaders.set("Access-Control-Allow-Origin","*")       
-  return newHeaders;
+export async function onRequest(context) {
+  const { request } = context;
+  const parsedUrl = new URL(request.url);
+  let path = parsedUrl.pathname.replace("/stream", "");
+  let search = parsedUrl.search;
+  let last = path + search;
+
+  let response = await fetch("https://esraerol2.volestream.lat/" + last);
+
+  const headers = new Headers(response.headers);
+  headers.delete("set-cookie");
+  headers.set("Access-Control-Allow-Origin", "*");
+  headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  headers.set("Access-Control-Allow-Headers", "Content-Type");
+
+  return new Response(response.body, {
+    status: response.status,
+    headers,
+  });
 }
-
-async function handleRequest(request) {
-  const parsedUrl = new URL(request.url)
-  let path = parsedUrl.pathname.replace("/stream","")
-  let search = parsedUrl.search
-  var last = path + search
-
-  let response = await fetch("https://esraerol2.volestream.lat/" + last)
-
-  return new Response(
-        response.body,
-        {
-            status: response.status,
-          
-            headers: translateResponseLocation(response.headers)
-        }
-    )
-  
-}
-
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
